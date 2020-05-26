2b87e125109468ca45a96c49ffeb093955b582ff
Home <allosipyan@mail.ru>
2020-05-26 15:27:40 +0500

    tests

--- src/test/java/WeatherRequest/tests/MyTests.java
+++ src/test/java/WeatherRequest/tests/MyTests.java
@@ -22,7 +22,7 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class MyTests extends BaseTest{
     @Test()
-    public void dataProperties(){
+    public void checkCityParam(){
         WeatherDataService weatherDataService = new WeatherDataService(properties);
         DateFormat df = new SimpleDateFormat("yyyy-MM-dd");
         df.setTimeZone(TimeZone.getTimeZone("Europe/London"));
